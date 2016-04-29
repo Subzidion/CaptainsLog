266d367bded4d2efe199ca049b527f5a0a9e1607
Carl 'Subzidion' Hiltbrunner <carlhiltbrunner@hotmail.com>
2016-04-29 16:40:10 -0400

    Created gitignore file, started working on options handling. Can now do basic dump of database.

--- captainsLog.sh
+++ captainsLog.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-#CREATE NEW ENTRY
-  sqlite3 captainslog.db  "CREATE TABLE IF NOT EXISTS log (id INTEGER PRIMARY KEY,date TEXT, log TEXT);"
+function CREATE {
+  sqlite3 ~/.captainslog/captainslog.db  "CREATE TABLE IF NOT EXISTS log (id INTEGER PRIMARY KEY,date TEXT, log TEXT);"
   #Get Date for Database
-  DBDATE=$(sqlite3 captainslog.db "SELECT datetime('now');")
+  DBDATE=$(sqlite3 ~/.captainslog/captainslog.db "SELECT datetime('now');")
   #Get Date and Time for Print Statement
   DATE=$(date '+%d-%b-%Y')
   TIME=$(date '+%r')
@@ -14,6 +14,33 @@
   #Replace single quotes with two single quotes because sqlite3 strings
   DESCRIPTION=${DESCRIPTION//\'/\'\'}
   #Add Entry to Database
-  sqlite3 captainslog.db  "INSERT INTO log (date, log) VALUES ('${DBDATE}', '${DESCRIPTION}');"
+  sqlite3 ~/.captainslog/captainslog.db  "INSERT INTO log (date, log) VALUES ('${DBDATE}', '${DESCRIPTION}');"
+}
 
-#DUMP DATABASE
+function PRINT {
+  sqlite3 ~/.captainslog/captainslog.db "SELECT * FROM log"
+}
+
+function HELP {
+  echo "-p to print, -h for help"
+}
+
+if [ $# == 0 ]
+  then
+  CREATE
+  else
+  while getopts ::ph FLAG; do
+    case $FLAG in
+    p)
+      PRINT
+      ;;
+    h)
+      HELP
+      ;;
+    \?)
+      echo "Unrecognized!"
+      HELP
+      ;;
+    esac
+  done 
+fi
